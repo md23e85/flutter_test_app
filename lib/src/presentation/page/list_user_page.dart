5eb47e69a3a27d3de4fd2a43618611be02ca2a68
Max <md23e85@gmail.com>
2019-08-14 23:21:49 +0300

    add group user list

--- lib/src/presentation/page/list_user_page.dart
+++ lib/src/presentation/page/list_user_page.dart
@@ -10,8 +10,6 @@ class ListUserPage extends StatefulWidget {
 
 class ListUserPageState extends State<ListUserPage> {
   ListUserBloc _bloc = $Provider.of<ListUserBloc>();
-  ScrollController _scrollController = new ScrollController();
-  bool _isLoading = false;
 
   @override
   void initState() {
@@ -27,33 +25,58 @@ class ListUserPageState extends State<ListUserPage> {
                 stream: _bloc.users,
                 builder: (context, snapshot) {
                   if (snapshot.connectionState == ConnectionState.waiting) {
-                    return Container(child: CircularProgressIndicator(), alignment: Alignment.center);
+                    return Container(
+                        child: CircularProgressIndicator(),
+                        alignment: Alignment.center);
                   }
                   if (snapshot.connectionState == ConnectionState.active) {
-                    _isLoading = false;
-                    return NotificationListener(
-                        onNotification: (ScrollNotification notification) {
-                          if (notification.metrics.pixels ==
-                                  notification.metrics.maxScrollExtent &&
-                              !_isLoading) {
-                            _isLoading = true;
-                            _bloc.uploadListUser();
+                    return ListView.builder(
+                        itemBuilder: (BuildContext context, int index) {
+                          if (index == 0) return _contactItem(snapshot.data[index], true);
+                          if (snapshot.data[index].name[0] != snapshot.data[index - 1].name[0]) {
+                            return _contactItem(snapshot.data[index], true);
                           }
+                          return _contactItem(snapshot.data[index], false);
                         },
-                        child: ListView.builder(
-                            itemBuilder: (BuildContext context, int index) {
-                              return _listItem(snapshot.data[index]);
-                            },
-                            itemCount: snapshot.data.length,
-                            controller: _scrollController));
+                        itemCount: snapshot.data.length);
                   }
                 })));
   }
 }
 
-Widget _listItem(UserModel user) {
-  return Card(
-      child: Padding(
-          child: Text(user.name, style: TextStyle(fontSize: 36)),
-          padding: EdgeInsets.all(10)));
+Widget _contactItem(UserModel user, bool isSeparator) {
+  return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        isSeparator ? _separatorItem(user.name[0]) : SizedBox(),
+        Container(
+            margin: EdgeInsets.all(5),
+            padding: EdgeInsets.all(5),
+            color: Colors.grey[300],
+            child: Row(
+              children: <Widget>[
+                ClipOval(
+                    child: Container(
+                  color: Colors.blue,
+                  width: 50,
+                  height: 50,
+                  alignment: Alignment.center,
+                  child: Text(user.name[0],
+                      style: TextStyle(color: Colors.white, fontSize: 42)),
+                )),
+                Container(
+                    child: Text(user.name, style: TextStyle(fontSize: 34)),
+                    margin: EdgeInsets.only(left: 10))
+              ],
+            ))
+      ]);
+}
+
+Widget _separatorItem(String name) {
+  return Container(
+    color: Colors.orange,
+      width: double.infinity,
+      child:
+          Text(name, style: TextStyle(fontSize: 32), textAlign: TextAlign.left),
+      margin: EdgeInsets.only(left: 10, right: 10));
 }
--- lib/src/presentation/services/list_user_service.dart
+++ lib/src/presentation/services/list_user_service.dart
@@ -5,8 +5,6 @@ import 'package:test_app/src/presentation/model/user_model.dart';
 import 'grpc_client_singleton.dart';
 
 class ListUserService extends BaseService {
-  String _nextPageToken = "";
-  static final int _COUNT_PAGINATION = 50;
   List<UserModel> _uploadUsers = List();
 
   getListUser(Function function) async {
@@ -14,16 +12,11 @@ class ListUserService extends BaseService {
     UserModel user = await getUser();
     UsersServiceClient client = UsersServiceClient(channel,
         options: CallOptions(metadata: {"bearer": user.token}));
-    client
-        .listUsers(ListUsersRequest()
-          ..pageSize = _COUNT_PAGINATION
-          ..pageToken = _nextPageToken..orderBy = EnumOrderBy.DESC)
-        .then((resp) {
-      _nextPageToken = resp.nextPageToken;
+    client.listUsers(ListUsersRequest()).then((resp) {
       resp.users.forEach((user) {
         _uploadUsers.add(UserModel.fromUserResponse(user));
       });
-      _uploadUsers.sort((a,b) => a.name.compareTo(b.name));
+      _uploadUsers.sort((a, b) => a.name.compareTo(b.name));
       function(_uploadUsers);
     });
   }
